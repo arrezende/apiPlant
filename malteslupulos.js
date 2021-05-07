0e93634dd86efdeb56da09a6995fbe906aae9c3d
Alex Rodrigo Rezende <arrezende@gmail.com>
2021-05-07 15:37:39 -0300

    Update malteslupulos.js

--- malteslupulos.js
+++ malteslupulos.js
@@ -4,7 +4,7 @@ export const malteslupulos = [
         name: "Pilsen",
         about: "Como o nome sugere, esse tipo de malte é uma das maneiras mais simples de se obter uma pilsner tcheca ou alemã. Estes maltes são claros e têm um sabor delicado, tornando-os perfeitos para lagers e ales claras.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "base"
@@ -19,7 +19,7 @@ export const malteslupulos = [
         name: "Pale Ale",
         about: "Para não ser confundido com a variedade Pale, os maltes pale ale são desenvolvidos especificamente para as pale ales de estilo inglês. Eles são mais escuros do que maltes Pale e têm um sabor maltado mais forte.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm34.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm34.svg",
         color: "#F5F360",
         environments: [
           "base"
@@ -34,7 +34,7 @@ export const malteslupulos = [
         name: "Vienna ",
         about: "O malte vienna também é produzido igual ao malte pale ale, mas a temperatura de secagem é um pouco mais elevada. Sua cor fica em torno de 8 a 10 EBC e seu poder de conversão é levemente mais baixo que o pale ale. Sua taxa de conversão com receitas que utilizam muitos adjuntos ou maltes especiais é baixa. Foi desenvolvido especialmente para a receita de Vienna Lager, mas pode ser utilizado em outros estilos também.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm34.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm34.svg",
         color: "#F5F360",
         environments: [
          "base"
@@ -49,7 +49,7 @@ export const malteslupulos = [
         name: "Munique",
         about: "O malte munique é seco em altas temperaturas e pode chegar ter até 60 EBC em coloração. Raramente ele é usado sozinho como malte base, recomenda-se uma combinação de malte munique e malte pale ale e quando utilizado, ele dá uma sabor tostado à cerveja",
         water_tips: "",
-        photo: "http://192.168.1.127:3000/maltes/srm1014.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm1014.svg",
         color: "#F5F360",
         environments: [
           "base"
@@ -64,7 +64,7 @@ export const malteslupulos = [
         name: "Carapils",
         about: "malte bastante claro com coloração variando de 8 – 16 EBC. É bastante utilizado para melhorar a retenção de espuma na cerveja e dar uma sensação maior de corpo na boca. Proporciona um leve dulçor na cerveja",
         water_tips: "Até 30% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm69.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm69.svg",
         color: "#F5F360",
         environments: [
           "crystal"
@@ -79,7 +79,7 @@ export const malteslupulos = [
         name: "Torrados",
         about: "O nome da família é bastante autoexplicativa: estes maltes são obtidos torrando maltes pale. Embora isso diminua seu potencial de gerar açúcares, eles desenvolvem sabores secos, amargos e de cacau.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm1922.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm1922.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -94,7 +94,7 @@ export const malteslupulos = [
         name: "Biscuit",
         about: "O malte biscuit foi desenvolvido para proporcionar notas e aromas de biscoit e pão à cerveja. É levemente mais escuro que o malte aromático, com coloração em torno de 23-26°L. Possui poder diastático muito baixo.",
         water_tips: "Até 15% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm2230.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm2230.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -109,7 +109,7 @@ export const malteslupulos = [
         name: "Caramunich",
         about: "Malte com coloração média, variando de 80 a 100 EBC. É ideal para cervejas estilos Oktoberfest, bastante utilizado em bocks, doppelbocks e maibocks. Proporciona sensação corpo e sensação redonda na boca. Tem características de caramelo com um final de nozes.",
         water_tips: "Até 10% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm35.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm35.svg",
         color: "#F5F360",
         environments: [
           "crystal"
@@ -124,7 +124,7 @@ export const malteslupulos = [
         name: "Special B",
         about: "malte caramelizado bastante escuro, com coloração variando de 200 – 280 EBC. A principal característica é o aroma que proporciona á cerveja, aroma forte, quase de caramelo queimado. Bastante versátil, pode ser usado de pale ales a stouts.",
         water_tips: "Até 10% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm35.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm35.svg",
         color: "#F5F360",
         environments: [
           "crystal"
@@ -139,7 +139,7 @@ export const malteslupulos = [
         name: "Crystal",
         about: "existe uma grande gama de malte crystal e eles são similares aos maltes mencionados acima. A coloração do malte crystal varia de 10-20, 30, 40, 45, 50-60, 80- 90, 100-120, 130-150, e 160-180 Lovibond. Quanto mais claro o malte crystal for, menos aroma de caramelo ele terá e quanto mais escuro ele for, mais aroma de tostado e nozes. Também dentro dos maltes especiais, temos os maltes torrados, estes passam por processo de torrefação em altas temperaturas, o que resulta uma coloração bastante escura e notas de café e chocolate.",
         water_tips: "Até 20% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -154,7 +154,7 @@ export const malteslupulos = [
         name: "Chocolate",
         about: "o malte chocolate é seco em temperaturas muito altas, não possui poder diastático. Sua coloração varia entre 350-450°L. Seu aroma varia entre queimado e chocolate amargo. Muito utilizado em cervejas porters e stouts.",
         water_tips: "Até 7% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm35.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm35.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -170,7 +170,7 @@ export const malteslupulos = [
         name: "Black",
         about: "o malte black é um dos maltes mais escuros no Mercado, variando de 500-600°L. É utilizado em stouts e outras cervejas escuras. Ele proporciona sabor tostado e adstringente à cerveja e também tem a capacidade de abaixar o ph do mosto.",
         water_tips: "Entre 3 - 6% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm35.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm35.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -186,7 +186,7 @@ export const malteslupulos = [
         name: "Cevada Torrada",
         about: "A cevada torrada não é um malte, mas é muito utilizada pelos cervejeiros, ela tem sabor parecido com o malte black, porém possui menos adstringência. Sua coloração gira em torno de 450-600°L.",
         water_tips: "Até 0% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm35.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm35.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -201,7 +201,7 @@ export const malteslupulos = [
         name: "Malte Acidulado",
         about: "Também chamado de Sauermalz, alemão para malte azedo, o malte acidulado é usado para adicionar sabores azedos à cerveja. Esse malte é feito pulverizando o mosto azedo sobre a cevada enquanto ela está germinando. Ele é super potente, então use com moderação. Ele pode tornar a sua cerveja muito azeda e reduzir significativamente o pH da cerveja.",
         water_tips: "Cada 1% de malte, reduz 0,1 de pH. Até 10%",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -216,7 +216,7 @@ export const malteslupulos = [
         name: "Malte Defumado",
         about: "Após a germinação, o malte verde é completamente ou parcialmente seco usando um fogo de madeira para obter um sabor defumado. É possível utilizar madeiras diferentes para trazer diferentes sabores.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -231,7 +231,7 @@ export const malteslupulos = [
         name: "Trigo",
         about: "Eles têm um alto teor de proteína se você estiver procurando por uma espuma mais espessa e duradoura e um sabor mais forte.",
         water_tips: "Até 100% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -246,7 +246,7 @@ export const malteslupulos = [
         name: "Centeio",
         about: "funciona bem com saltos ousados e pode adicionar um sabor picante à sua bebida. Não possui casca, por isso pode ser difícil de realizar a lavagem.",
         water_tips: "Até 0% da receita em grãos",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
@@ -261,7 +261,7 @@ export const malteslupulos = [
         name: "Aveia",
         about: "Preenche o sabor da sua cerveja e dá uma sensação suave na boca adicionando corpo a sua cerveja.",
         water_tips: "Até 25% do total de grãos para cervejas escuras e até 5% para cervejas claras",
-        photo: "http://192.168.1.127:3000/maltes/srm23.svg",
+        photo: "https://api-plant.vercel.app/maltes/srm23.svg",
         color: "#F5F360",
         environments: [
           "outros"
