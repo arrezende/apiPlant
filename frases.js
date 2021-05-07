424fd795c7a17116e9715b3cc05b6dfc584a8de5
Alex Rodrigo Rezende <arrezende@gmail.com>
2021-05-07 15:35:04 -0300

    Add files via upload

---
+++ frases.js
@@ -0,0 +1,24 @@
+export const frases = [
+    {
+      id: '1',
+      content: 'Só é sexta-feira depois do primeiro copo de cerveja.',
+    },
+    {
+      id: '2',
+      content: 'Se quiser, pode partir o meu coração. Só não derruba a minha cerveja.',
+    },
+    {
+      id: '3',
+      content: 'Sem dúvida, a maior invenção da humanidade foi a cerveja. Tudo bem, a roda também foi importante, mas garanto que ela não desce tão bem acompanhada de uma pizza.',
+    },
+    {
+      id: '4',
+      content: 'Salve o planeta. Afinal, ele é o único que tem cerveja.',
+    },
+    {
+      id: '5',
+      content: 'Que o único fardo que tenhamos que carregar seja o fardo de cerveja!',
+    },
+    
+  ]
+  
